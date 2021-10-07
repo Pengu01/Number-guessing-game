f79450548f062246527daaabfdb0623b238967d3
Alfons Stoltz <alfons.stoltz@learnet.se>
2021-10-07 10:09:18 +0200

    Add project files.

---
+++ Number guessing/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Number_guessing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            int rightnumber = rnd.Next(100);
+            while (rightnumber < 101)
+            {
+                int guess = Convert.ToInt32(Console.ReadLine());
+                if (guess == rightnumber)
+                {
+                    Console.WriteLine("Correct, new number");
+                    rightnumber = rnd.Next(100);
+                }
+                else if (guess < rightnumber)
+                {
+                    Console.WriteLine("Too low");
+                }
+                else if (guess > rightnumber)
+                {
+                    Console.WriteLine("Too high");
+                    continue;
+                }
+            }
+        }
+    }
+}
